abf37ac75959d7228300d42c9cb48881ac30f8e3
lokeshRedEye <lokeshcoder123@gmail.com>
2025-07-04 19:23:07 +0530

    comapny

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -58,18 +58,17 @@ const Hero = () => {
 
       {/* truetse by sectiom */}
 
-    <div  className="border border-gray-400 shadow-md mx-2 mt-5 p-6 rounded-md  flex ">
-        <div className="flex justify-center gap:10 lg:gap-16 flex-wrap">
-            <p className="font-medium">Trusted by </p>
-            <img className="h-6" src={assets.microsoft_logo}/>
-            <img className="h-6" src={assets.walmart_logo}/>
-            <img className="h-6" src={assets.samsung_logo}/>
-            <img className="h-6" src={assets.accenture_logo}/>
-            <img className="h-6" src={assets.adobe_logo}/>
-            <img className="h-6" src={assets.amazon_logo}/>
-
+    <div className='border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex'>
+            <div className='flex justify-center gap-10 lg:gap-16 flex-wrap'>
+                <p className='font-medium'>Trusted by</p>
+                <img className='h-6' src={assets.accenture_logo} alt="" />
+                <img className='h-6' src={assets.walmart_logo} alt="" />
+                <img className='h-6' src={assets.samsung_logo} alt="" />
+                <img className='h-6' src={assets.amazon_logo} alt="" />
+                <img className='h-6' src={assets.microsoft_logo} alt="" />
+                <img className='h-6' src={assets.adobe_logo} alt="" />
+            </div>
         </div>
-    </div>
 
 
     </div>
